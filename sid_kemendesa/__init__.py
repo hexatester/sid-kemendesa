72e0c58411535f8665b452458b606e981c8c7c89
hexatester <hexatester@protonmail.com>
2021-05-03 21:13:47 +0700

    Added .search

--- sid_kemendesa/__init__.py
+++ sid_kemendesa/__init__.py
@@ -1,1 +1,13 @@
+from .search import ListResponse, HasilDesa, listKab, listKec, listDesa, search
+
 __version__ = "0.1.0"
+
+
+__all__ = [
+    "ListResponse",
+    "HasilDesa",
+    "listKab",
+    "listKec",
+    "listDesa",
+    "search",
+]
