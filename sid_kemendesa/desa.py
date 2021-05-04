537a690740477c5989fa2214bcaa992ff98b1946
hexatester <hexatester@protonmail.com>
2021-05-04 11:13:20 +0700

    Add Desa.idm

--- sid_kemendesa/desa.py
+++ sid_kemendesa/desa.py
@@ -3,6 +3,7 @@ import requests
 from typing import Optional
 
 from sid_kemendesa import DeskripsiDesa
+from sid_kemendesa import IdmDesa
 from sid_kemendesa.constants import BASE_URL
 
 
@@ -10,6 +11,7 @@ from sid_kemendesa.constants import BASE_URL
 class Desa:
     iddesa: str
     _deskripsi: Optional[DeskripsiDesa] = None
+    _idm: Optional[IdmDesa] = None
 
     @property
     def deskripsi(self) -> DeskripsiDesa:
@@ -17,3 +19,10 @@ class Desa:
             return self._deskripsi
         self._deskripsi = DeskripsiDesa.from_iddesa(self.iddesa)
         return self._deskripsi
+
+    @property
+    def idm(self) -> IdmDesa:
+        if self._idm:
+            return self._idm
+        self._idm = IdmDesa.from_iddesa(self.iddesa)
+        return self._idm
