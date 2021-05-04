f94b1e4fd6257e358a7b7734ed6b7b1980f2310b
hexatester <hexatester@protonmail.com>
2021-05-04 12:06:02 +0700

    Bumping version from 0.1.1 to 0.2.0

--- sid_kemendesa/__init__.py
+++ sid_kemendesa/__init__.py
@@ -3,7 +3,7 @@ from .idm import IdmDesa
 from .desa import Desa
 from .search import ListResponse, HasilDesa, listKab, listKec, listDesa, search
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 __all__ = [
